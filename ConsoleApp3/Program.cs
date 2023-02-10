7de32d5f0572d5bc8223eb3bef3f5b8047c1b6af
Bergqvist1 <edvin.sven123@gmail.com>
2023-02-10 10:44:20 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Störst (7,12);
+
+        }
+        static void Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                Console.WriteLine($"talet {tal1} är större än {tal2}");
+            }
+
+
+            else if (tal1 < tal2) 
+            {
+                Console.WriteLine($"talet {tal2} är större än {tal1}");
+            }
+
+            else 
+            {
+                Console.WriteLine($"Talen {tal1} och {tal2} är lika stora");
+
+            }       
+        }
+    }
+}
